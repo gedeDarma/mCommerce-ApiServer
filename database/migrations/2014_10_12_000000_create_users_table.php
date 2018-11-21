5b6fa6635789a77de3c251dc63ad5791af06a0ec
unknown <de.darma.damuh@gmail.com>
2018-11-21 16:29:44 +0800

    modified user and admin table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,10 +20,10 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->string('phone');
-            $table->text('address');
-            $table->string('photo');
-            $table->json('meta');
+            $table->string('phone')->nullable();;
+            $table->text('address')->nullable();;
+            $table->string('photo')->nullable();;
+            $table->json('meta')->nullable();;
             $table->timestamps();
         });
     }
